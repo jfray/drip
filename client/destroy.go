6b9f3810bae1b5bae3c98de291a9791c04497432
Joshua Fraser <jfraser@apcera.com>
2015-08-16 02:22:03 -0700

    This refactor allowed for better use of subcommands and args specifically tied to the subcommand. I utilized the spf13/cobra library, which is pretty damned nice. This commit closes #7.

--- client/destroy.go
+++ client/destroy.go
@@ -1,39 +1,11 @@
 package drip_client
 
-import (
-	"fmt"
-	"strings"
+import "github.com/digitalocean/godo"
 
-	"github.com/digitalocean/godo"
-)
-
-func (cc *DripClient) Destroy(IDs ...int) ([]*godo.Response, error) {
-	allResponses := make([]*godo.Response, 0)
-	allErrors := make([]string, 0)
-
-	// This is an actual fatal error in this case, so let's bomb out
-	if len(IDs) > cc.MainConfig.MaxHosts {
-		return nil, fmt.Errorf(
-			"Some people just want to see the world burn. Others just " +
-				"mistype how many machines they want to delete concurrently." +
-				"Bill from Steel Mountain just wants to take instagram pics " +
-				"of him and his cats. Config is set to %d right now.",
-		)
-
+func (cc *DripClient) Destroy(ID int) (*godo.Response, error) {
+	resp, err := cc.Client.Droplets.Delete(ID)
+	if err != nil {
+		return nil, err
 	}
-
-	// We don't necessarily want to error out the entire function if not all
-	// hosts are destroyed. Let's keep track of both indepedently and maybe
-	// requeue the failures?
-	//
-	// TODO: figure out a good queue strategy
-	for _, ID := range IDs {
-		resp, err := cc.Client.Droplets.Delete(ID)
-		if err != nil {
-			allErrors = append(allErrors, err.Error())
-		}
-		allResponses = append(allResponses, resp)
-	}
-
-	return allResponses, fmt.Errorf(strings.Join(allErrors, ","))
+	return resp, nil
 }
--- drip.go
+++ drip.go
@@ -9,11 +9,13 @@ import (
 	"io/ioutil"
 	"log"
 	"path/filepath"
+	"strconv"
 	"strings"
 
 	"github.com/jfray/drip/auth"
 	"github.com/jfray/drip/client"
 	"github.com/jfray/drip/config"
+	"github.com/spf13/cobra"
 )
 
 var (
@@ -21,11 +23,6 @@ var (
 		"cluster",
 		"sfo1-1",
 		"which cluster are we working with?",
-	)
-	howMany = flag.Int(
-		"how_many",
-		1,
-		"how many machines should we build/destroy/whatever?",
 	)
 	datacenter = strings.Split(*cluster, "-")[0]
 	confDir    = "./.drip"
@@ -96,34 +93,102 @@ func main() {
 	}
 	// COMMON PORTION BETWEEN ALL SUBCOMMANDS
 
-	// need to figure out how to do the whole subcommand thing
-	//log.Printf("Create %d hosts", *howMany)
-	//droplets, err := cc.Create(*howMany)
-	//if err != nil {
-	//		log.Fatalf("Could not create droplets: %q", err)
-	//	}
-	//	for _, droplet := range droplets {
-	//		log.Printf("here is what I have for create: %+v", droplet)
-	//	}
-
-	list, err := cc.List()
-	for _, l := range list {
-		log.Printf("Here is my list of names/IDs: %s %d", l.Name, l.ID)
+	var listCmd = &cobra.Command{
+		Use:   "list",
+		Short: "List running cluster instances",
+		Long:  `List all running instances for the chosen cluster`,
+		Run: func(cmd *cobra.Command, args []string) {
+			ccList, err := cc.List()
+			if err != nil {
+				log.Fatalf("Could not retrieve droplet list: %q", err)
+			}
+			for _, l := range ccList {
+				log.Printf("Name: %s, ID: %d", l.Name, l.ID)
+			}
+		},
 	}
 
-	//log.Println("SHOW 6605949")
-	//cc.Show(6605949)
+	var machineCount int
+	var createCmd = &cobra.Command{
+		Use:   "create",
+		Short: "Create one or more new instances",
+		Long: `this will build one or more new amachines based on the existin
+		g cluster configuration`,
+		Run: func(cmd *cobra.Command, args []string) {
+			droplets, err := cc.Create(machineCount)
+			if err != nil {
+				log.Fatalf("Could not create droplets: %q", err)
+			}
+			for _, droplet := range droplets {
+				log.Printf(
+					"Created. Name: %s, ID: %d, DATA: %+v",
+					droplet.Name,
+					droplet.ID,
+					droplet,
+				)
+			}
+		},
+	}
 
-	/*
-		log.Println("DESTROY ALL")
-		responses, err := cc.Destroy(6620248, 6620249, 6620250)
-		if err != nil {
-			log.Fatalf("Could not get all of them: %q", err)
-		}
+	createCmd.Flags().IntVarP(
+		&machineCount,
+		"machine_count",
+		"m",
+		1,
+		"How many machines to build at a time.",
+	)
 
-		for _, response := range responses {
+	var showCmd = &cobra.Command{
+		Use:   "show",
+		Short: "Show information about a specific instance.",
+		Long: `get all information currently known about an image, keyed by the 
+		image ID`,
+		Run: func(cmd *cobra.Command, args []string) {
+			ID, err := strconv.Atoi(strings.Join(args, " "))
+			if err != nil {
+				log.Fatalf(
+					"Could not convert argument %s to valid image ID (%q)",
+					strings.Join(args, " "),
+					err,
+				)
+			}
+
+			droplet, err := cc.Show(ID)
+			if err != nil {
+				log.Fatalf("Could not access image ID: %d (%q)", ID, err)
+			}
+
+			log.Printf(
+				"Name: %s, ID: %d, DATA: %+v",
+				droplet.Name,
+				droplet.ID,
+				droplet,
+			)
+		},
+	}
+
+	var destroyCmd = &cobra.Command{
+		Use:   "destroy",
+		Short: "Destroy images by ID",
+		Long:  `destroy the image referenced by their ID`,
+		Run: func(cmd *cobra.Command, args []string) {
+			ID, err := strconv.Atoi(strings.Join(args, " "))
+			if err != nil {
+				log.Fatalf(
+					"Could not convert arguments %s to valid image ID (%q)",
+					strings.Join(args, " "),
+					err,
+				)
+			}
+			response, err := cc.Destroy(ID)
+			if err != nil {
+				log.Fatalf("Could not delete image ID: %d (%q)", ID, err)
+			}
 			log.Printf("Response: %s", response)
-		}
-	*/
+		},
+	}
 
+	var rootCmd = &cobra.Command{Use: "app"}
+	rootCmd.AddCommand(listCmd, createCmd, showCmd, destroyCmd)
+	rootCmd.Execute()
 }
